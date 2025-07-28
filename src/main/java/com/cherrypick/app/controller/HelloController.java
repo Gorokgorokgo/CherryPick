98f39ab0e3b4a5e3f42f4e5e99691d8fc1688ec4
Gorokgorokgo <dydwnsl02@naver.com>
2025-07-28 10:11:55 +0900

    fix: nakchal -> cherrypick으로 변경 (#1)

--- src/main/java/com/cherrypick/app/controller/HelloController.java
+++ src/main/java/com/cherrypick/app/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.nakchal.app.controller;
+package com.cherrypick.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello, Nakchal!";
+        return "Hello, Cherrypick!";
     }
     
     @GetMapping("/api/health")
