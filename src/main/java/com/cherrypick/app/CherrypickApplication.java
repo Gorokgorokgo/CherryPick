3ebcf5c671fc2c509f6516bfd41661193ae57526
Park_Yong_Jun <dydwnsl02@naver.com>
2025-08-03 15:02:33 +0900

    feat: .env 파일 자동 로드 설정 추가

--- src/main/java/com/cherrypick/app/CherrypickApplication.java
+++ src/main/java/com/cherrypick/app/CherrypickApplication.java
@@ -1,12 +1,19 @@
 package com.cherrypick.app;
 
+import me.paulschwarz.springdotenv.environment.DotenvPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class CherrypickApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CherrypickApplication.class, args);
+        SpringApplication app = new SpringApplication(CherrypickApplication.class);
+        app.addInitializers(applicationContext -> {
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            environment.getPropertySources().addFirst(new DotenvPropertySource());
+        });
+        app.run(args);
     }
 }
