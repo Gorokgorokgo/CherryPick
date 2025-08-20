6675d4b2b8a98749c9ff4338c009674286b92fe8
yongjun <dydwnsl02@naver.com>
2025-08-20 12:22:42 +0900

    임시 커밋

---
+++ mobile/react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {
+      sourceDir: './android',
+      appName: 'app',
+      packageName: 'com.cherrypickmobile'
+    },
+  },
+  assets: ['./src/assets/fonts/'],
+  dependencies: {},
+};
